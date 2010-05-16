b6b47a3bd904c47e279567f4576ab008b9c7fbcd
RevolutionSmythe <asdfisbetterthanjkl@gmail.com>
2010-05-16 00:30:31 -0600

    Fixes a bug in selecting a map tile.

--- OpenSim/Server/Handlers/AuroraMap/WorldMapPostHandler.cs
+++ OpenSim/Server/Handlers/AuroraMap/WorldMapPostHandler.cs
@@ -90,40 +90,40 @@ namespace OpenSim.Server.Handlers.AuroraMap
             int regionXMax = int.Parse(request["REGIONLOCXMAX"].ToString());
             int regionYMax = int.Parse(request["REGIONLOCYMAX"].ToString());
 
-            //List<Services.Interfaces.GridRegion> Regions = GridService.GetRegionRange(UUID.Zero,
-            //        regionXMin,
-            //        regionYMin,
-            //        regionXMax,
-            //        regionYMax);
+            List<Services.Interfaces.GridRegion> Regions = GridService.GetRegionRange(UUID.Zero,
+                    regionXMin,
+                    regionYMin,
+                    regionXMax,
+                    regionYMax);
 
-            //foreach (Services.Interfaces.GridRegion region in Regions)
-            //{
-            //    SimMap map = SimMapConnector.GetSimMap(region.RegionID, agentID);
-            //    if (map != null)
-            //        Sims.Add(map);
-            //}
-
-            //if (Sims.Count == 0)
-            //    Sims.Add(SimMapConnector.NotFound(regionXMin, regionYMin));
-            int X = regionXMin;
-            int Y = regionYMin;
-            while(X < regionXMax)
+            foreach (Services.Interfaces.GridRegion region in Regions)
             {
-                while (Y < regionYMax)
-                {
-                    Services.Interfaces.GridRegion R = GridService.GetRegionByPosition(UUID.Zero, X, Y);
-                    if (R == null)
-                        Sims.Add(SimMapConnector.NotFound(X, Y));
-                    else
-                    {
-                        SimMap map = SimMapConnector.GetSimMap(R.RegionID, agentID);
-                        Sims.Add(map);
-                    }
-                    Y += 256;
-                }
-                Y = regionYMin;
-                X += 256;
+                SimMap map = SimMapConnector.GetSimMap(region.RegionID, agentID);
+                if (map != null)
+                    Sims.Add(map);
             }
+
+            if (Sims.Count == 0)
+                Sims.Add(SimMapConnector.NotFound(regionXMin, regionYMin));
+            //int X = regionXMin;
+            //int Y = regionYMin;
+            //while(X < regionXMax)
+            //{
+            //    while (Y < regionYMax)
+            //    {
+            //        Services.Interfaces.GridRegion R = GridService.GetRegionByPosition(UUID.Zero, X, Y);
+            //        if (R == null)
+            //            Sims.Add(SimMapConnector.NotFound(X, Y));
+            //        else
+            //        {
+            //            SimMap map = SimMapConnector.GetSimMap(R.RegionID, agentID);
+            //            Sims.Add(map);
+            //        }
+            //        Y += 256;
+            //    }
+            //    Y = regionYMin;
+            //    X += 256;
+            //}
 
             int i = 0;
             foreach (SimMap map in Sims)
