0fed1f9d057917c692f5e5d1e686628d1be66145
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2010-08-13 21:01:10 +0100

    refactor: remove Scene.SetRootAgentScene() in favour of existing event with same name

--- OpenSim/Tools/Robust.32BitLaunch/Properties/AssemblyInfo.cs
+++ OpenSim/Tools/Robust.32BitLaunch/Properties/AssemblyInfo.cs
@@ -25,39 +25,16 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using OpenMetaverse;
+using OpenSim.Region.Framework.Scenes;
 
-// General information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Robust.32BitLaunch")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("http://opensimulator.org")]
-[assembly: AssemblyProduct("Robust.32BitLaunch")]
-[assembly: AssemblyCopyright("Copyright (c) 2008")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5072e919-46ab-47e6-8a63-08108324ccdf")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.6.3.*")]
-[assembly: AssemblyVersion("0.6.3.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+namespace OpenSim.Region.Framework.Interfaces
+{
+    /// <summary>
+    /// An interface for a module that manages inter-agent inventory offers and transfers.
+    /// </summary>
+    public interface IInventoryTransferModule
+    {
+        bool NeedSceneCacheClear(UUID agentID, Scene scene);
+    }
+}
