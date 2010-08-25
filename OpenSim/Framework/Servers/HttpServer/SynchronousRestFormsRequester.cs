f057e24e754a8abd947e8f6edb5c23cce65f7b7f
Melanie Thielker <melanie@t-data.com>
2010-08-25 23:04:12 +0200

    Change some e.Message to e.ToString. Don't use e.Message, it doesn't carry any useful information. Error messages are useless without location information. It looks more elegant, but is totally pointless.

--- OpenSim/Framework/Servers/HttpServer/SynchronousRestFormsRequester.cs
+++ OpenSim/Framework/Servers/HttpServer/SynchronousRestFormsRequester.cs
@@ -81,7 +81,7 @@ namespace OpenSim.Framework.Servers.HttpServer
                 }
                 catch (Exception e)
                 {
-                    //m_log.DebugFormat("[FORMS]: exception occured on sending request to {0}: {1}", requestUrl, e.Message);
+                    m_log.DebugFormat("[FORMS]: exception occured on sending request to {0}: " + e.ToString(), requestUrl);
                 }
                 finally
                 {
@@ -112,7 +112,7 @@ namespace OpenSim.Framework.Servers.HttpServer
                         }
                         catch (Exception e)
                         {
-                            m_log.DebugFormat("[FORMS]: exception occured on receiving reply {0}", e.Message);
+                            m_log.DebugFormat("[FORMS]: exception occured on receiving reply " + e.ToString());
                         }
                         finally
                         {
@@ -125,7 +125,7 @@ namespace OpenSim.Framework.Servers.HttpServer
             catch (System.InvalidOperationException)
             {
                 // This is what happens when there is invalid XML
-                //m_log.DebugFormat("[FORMS]: InvalidOperationException on receiving request");
+                m_log.DebugFormat("[FORMS]: InvalidOperationException on receiving request");
             }
             return respstring;
         }
