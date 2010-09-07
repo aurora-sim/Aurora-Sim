d782076d79c14879dd3af42b15077913b0ad4f8d
John Hurliman <john.hurliman@intel.com>
2010-09-07 13:43:36 -0700

    * Added sanity checks to the SimianAssetServiceConnector so it will throw a sensible error message when it is misconfigured instead of a mysterious "Uri is empty" error

--- OpenSim/Services/Connectors/SimianGrid/SimianAssetServiceConnector.cs
+++ OpenSim/Services/Connectors/SimianGrid/SimianAssetServiceConnector.cs
@@ -119,6 +119,12 @@ namespace OpenSim.Services.Connectors.SimianGrid
 
         public AssetBase Get(string id)
         {
+            if (String.IsNullOrEmpty(m_serverUrl))
+            {
+                m_log.Error("[SIMIAN ASSET CONNECTOR]: No AssetServerURI configured");
+                throw new InvalidOperationException();
+            }
+
             // Cache fetch
             if (m_cache != null)
             {
@@ -145,6 +151,12 @@ namespace OpenSim.Services.Connectors.SimianGrid
         /// <returns></returns>
         public AssetMetadata GetMetadata(string id)
         {
+            if (String.IsNullOrEmpty(m_serverUrl))
+            {
+                m_log.Error("[SIMIAN ASSET CONNECTOR]: No AssetServerURI configured");
+                throw new InvalidOperationException();
+            }
+
             AssetMetadata metadata = null;
 
             // Cache fetch
@@ -216,6 +228,12 @@ namespace OpenSim.Services.Connectors.SimianGrid
         /// <returns>True if the id was parseable, false otherwise</returns>
         public bool Get(string id, Object sender, AssetRetrieved handler)
         {
+            if (String.IsNullOrEmpty(m_serverUrl))
+            {
+                m_log.Error("[SIMIAN ASSET CONNECTOR]: No AssetServerURI configured");
+                throw new InvalidOperationException();
+            }
+
             // Cache fetch
             if (m_cache != null)
             {
@@ -246,6 +264,12 @@ namespace OpenSim.Services.Connectors.SimianGrid
         /// <returns></returns>
         public string Store(AssetBase asset)
         {
+            if (String.IsNullOrEmpty(m_serverUrl))
+            {
+                m_log.Error("[SIMIAN ASSET CONNECTOR]: No AssetServerURI configured");
+                throw new InvalidOperationException();
+            }
+
             bool storedInCache = false;
             string errorMessage = null;
 
@@ -378,6 +402,12 @@ namespace OpenSim.Services.Connectors.SimianGrid
         /// <returns></returns>
         public bool Delete(string id)
         {
+            if (String.IsNullOrEmpty(m_serverUrl))
+            {
+                m_log.Error("[SIMIAN ASSET CONNECTOR]: No AssetServerURI configured");
+                throw new InvalidOperationException();
+            }
+
             //string errorMessage = String.Empty;
             string url = m_serverUrl + id;
 
