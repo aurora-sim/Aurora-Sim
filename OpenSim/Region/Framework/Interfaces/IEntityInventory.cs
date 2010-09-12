395132106ea9064577936a2dee5dcb8456c7352d
John Hurliman <john.hurliman@intel.com>
2010-09-12 14:20:26 -0700

    * Added ISimulationDataService and IEstateDataService * Removed StorageManager * CONFIG CHANGE: There are no more database settings in OpenSim.ini. Check the config-include configuration files for region store and estate store database settings

--- OpenSim/Region/Framework/Interfaces/IEntityInventory.cs
+++ OpenSim/Region/Framework/Interfaces/IEntityInventory.cs
@@ -209,7 +209,7 @@ namespace OpenSim.Region.Framework.Interfaces
         /// Backup the inventory to the given data store
         /// </summary>
         /// <param name="datastore"></param>
-        void ProcessInventoryBackup(ISimulationDataStore datastore);
+        void ProcessInventoryBackup(ISimulationDataService datastore);
 
         uint MaskEffectivePermissions();
 
