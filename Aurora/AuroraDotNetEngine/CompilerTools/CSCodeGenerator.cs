93f49b22a5c4e23fec84e5617153416a3a06947d
RevolutionSmythe <asdfisbetterthanjkl@gmail.com>
2010-11-10 22:32:20 -0600

    Fix build....

--- Aurora/AuroraDotNetEngine/CompilerTools/CSCodeGenerator.cs
+++ Aurora/AuroraDotNetEngine/CompilerTools/CSCodeGenerator.cs
@@ -34,6 +34,90 @@ using Tools;
 
 namespace Aurora.ScriptEngine.AuroraDotNetEngine.CompilerTools
 {
+    public static class Extension
+    {
+        public static bool CompareWildcards(this string WildString, string Mask, bool IgnoreCase)
+        {
+            int i = 0;
+
+            if (String.IsNullOrEmpty(Mask))
+                return false;
+            if (Mask == "*")
+                return true;
+
+            while (i != Mask.Length)
+            {
+                if (CompareWildcard(WildString, Mask.Substring(i), IgnoreCase))
+                    return true;
+
+                while (i != Mask.Length && Mask[i] != ';')
+                    i += 1;
+
+                if (i != Mask.Length && Mask[i] == ';')
+                {
+                    i += 1;
+
+                    while (i != Mask.Length && Mask[i] == ' ')
+                        i += 1;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool CompareWildcard(this string WildString, string Mask, bool IgnoreCase)
+        {
+            int i = 0, k = 0;
+
+            while (k != WildString.Length)
+            {
+                if ((i + 1) > Mask.Length)
+                    return true;
+                switch (Mask[i])
+                {
+                    case '*':
+
+                        if ((i + 1) >= Mask.Length)
+                            return true;
+
+                        while (k != WildString.Length)
+                        {
+                            if (CompareWildcard(WildString.Substring(k + 1), Mask.Substring(i + 1), IgnoreCase))
+                                return true;
+
+                            k += 1;
+                        }
+
+                        return false;
+
+                    case '?':
+
+                        break;
+
+                    default:
+
+                        if (IgnoreCase == false && WildString[k] != Mask[i])
+                            return false;
+                        if (IgnoreCase && Char.ToLower(WildString[k]) != Char.ToLower(Mask[i]))
+                            return false;
+
+                        break;
+                }
+
+                i += 1;
+                k += 1;
+            }
+
+            if (k == WildString.Length)
+            {
+                if (i == Mask.Length || Mask[i] == ';' || Mask[i] == '*')
+                    return true;
+            }
+
+            return false;
+        }
+    }
+
     public class CSCodeGenerator : ICSCodeGenerator
     {
         private SYMBOL m_astRoot = null;
