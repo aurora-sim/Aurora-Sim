4c45dc8edf8f06823e7b22d312cf3a205fc334ba
RevolutionSmythe <asdfisbetterthanjkl@gmail.com>
2010-12-16 12:51:46 -0600

    Commit missing file!

---
+++ OpenSim/Region/Framework/Interfaces/IPhysicsMonitor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenMetaverse;
+using OpenSim.Region.Physics.Manager;
+
+namespace OpenSim.Region.Framework.Interfaces
+{
+    public interface IPhysicsMonitor
+    {
+        void AddPhysicsStats(UUID regionID, PhysicsScene scene);
+    }
+}
