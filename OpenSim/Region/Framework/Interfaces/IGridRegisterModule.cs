8ec64342ac8923a66a3d9c61faaa487a38acc16d
RevolutionSmythe <asdfisbetterthanjkl_AT_gmail.com>
2010-12-29 20:21:38 -0600

    Commit a forgotten file from two commits ago.

---
+++ OpenSim/Region/Framework/Interfaces/IGridRegisterModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenSim.Region.Framework.Scenes;
+
+namespace OpenSim.Region.Framework.Interfaces
+{
+    public interface IGridRegisterModule
+    {
+        /// <summary>
+        /// Update the grid server with new info about this region
+        /// </summary>
+        /// <param name="scene"></param>
+        void UpdateGridRegion(Scene scene);
+
+        /// <summary>
+        /// Register this region with the grid service
+        /// </summary>
+        /// <param name="scene"></param>
+        void RegisterRegionWithGrid(Scene scene);
+    }
+}
