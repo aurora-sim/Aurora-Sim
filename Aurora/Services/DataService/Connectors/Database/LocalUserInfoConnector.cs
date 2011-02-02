039cfcf472d0c2b327f1b3d2f69bfd8d4bd6605d
RevolutionSmythe <asdfisbetterthanjkl_AT_gmail.com>
2011-02-02 15:41:33 -0600

    Commit whatevers left.

--- Aurora/Services/DataService/Connectors/Database/LocalUserInfoConnector.cs
+++ Aurora/Services/DataService/Connectors/Database/LocalUserInfoConnector.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 using Aurora.Framework;
 using Aurora.DataManager;
 using OpenMetaverse;
