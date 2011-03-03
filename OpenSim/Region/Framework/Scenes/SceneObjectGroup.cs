a8c7560ff3fb2abc7f09bd0c040410da8ed56d12
UbitUmarov <ajlduarte@sapo.pt>
2011-03-03 07:32:23 +0000

    ooops

--- OpenSim/Region/Framework/Scenes/SceneObjectGroup.cs
+++ OpenSim/Region/Framework/Scenes/SceneObjectGroup.cs
@@ -843,19 +843,50 @@ namespace OpenSim.Region.Framework.Scenes
         /// <returns></returns>
         public void GetAxisAlignedBoundingBoxRaw(out float minX, out float maxX, out float minY, out float maxY, out float minZ, out float maxZ)
         {
-
             Vector3 pos = m_rootPart.AbsolutePosition;
+            Quaternion rot = m_rootPart.RotationOffset;
             Vector3 size = GroupScale();
+            Vector3 minScale = new Vector3(int.MaxValue, int.MaxValue, int.MaxValue);
+            Vector3 maxScale = new Vector3(int.MinValue, int.MinValue, int.MinValue);
+            //limits in group frame
+            lock (m_partsLock)
+                {
+                foreach (SceneObjectPart part in m_partsList)
+                    {
+                    Vector3 partscale = part.Scale * 0.5f;
+                    Vector3 partoffset = part.OffsetPosition;
+                    if (part.ParentID != 0) // prims are rotated in group
+                        partscale = partscale * part.RotationOffset;
 
-            size = size * m_rootPart.RotationOffset;
-            size *= 0.5f;
+                    Vector3 delta = partoffset - partscale;
+                    if (delta.X < minScale.X)
+                        minScale.X = delta.X;
+                    if (delta.Y < minScale.Y)
+                        minScale.Y = delta.Y;
+                    if (delta.Z < minScale.Z)
+                        minScale.Z = delta.Z;
 
-            minX = pos.X - size.X;
-            minY = pos.Y - size.Y;
-            minZ = pos.Z - size.Z;
-            maxX = pos.X + size.X;
-            maxY = pos.Y + size.Y;
-            maxZ = pos.Z + size.Z;
+                    delta = partoffset + partscale;
+                    if (delta.X > maxScale.X)
+                        maxScale.X = delta.X;
+                    if (delta.Y > maxScale.Y)
+                        maxScale.Y = delta.Y;
+                    if (delta.Z > maxScale.Z)
+                        maxScale.Z = delta.Z;
+                    }
+                }
+
+            // group rotation
+            maxScale = maxScale * rot;
+            minScale = minScale * rot;
+
+            // group position
+            minX = pos.X + minScale.X;
+            minY = pos.Y + minScale.Y;
+            minZ = pos.Z + minScale.Z;
+            maxX = pos.X + maxScale.X;
+            maxY = pos.Y + maxScale.Y;
+            maxZ = pos.Z + maxScale.Z;
 
 /*
             maxX = -256f;
