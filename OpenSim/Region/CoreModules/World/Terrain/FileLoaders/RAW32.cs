945fabacaf006fcf2473d5b46436b68435124002
RevolutionSmythe <asdfisbetterthanjkl_AT_gmail.com>
2011-03-29 23:29:15 -0500

    Fix a bug with R32 loading that was causing OAR loading with offsets into larger regions to fail.

--- OpenSim/Region/CoreModules/World/Terrain/FileLoaders/RAW32.cs
+++ OpenSim/Region/CoreModules/World/Terrain/FileLoaders/RAW32.cs
@@ -117,9 +117,10 @@ namespace OpenSim.Region.CoreModules.World.Terrain.FileLoaders
 
         public ITerrainChannel LoadStream (Stream s, IScene scene)
         {
-            TerrainChannel retval = new TerrainChannel (true, scene);
-
             BinaryReader bs = new BinaryReader(s);
+            int size = (int)System.Math.Sqrt(s.Length);
+            size = size > scene.RegionInfo.RegionSizeX ? scene.RegionInfo.RegionSizeX : size; //Chose the smaller of the two
+            TerrainChannel retval = new TerrainChannel(size, size, scene);
             int y;
             for (y = 0; y < retval.Height; y++)
             {
