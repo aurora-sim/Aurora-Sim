41ed300d86a89574ce3836e39d922b10fdf2830a
RevolutionSmythe <asdfisbetterthanjkl_AT_gmail.com>
2011-04-11 17:26:49 -0500

    Commit missing file.

---
+++ OpenSim/Region/Framework/Interfaces/IActivityDetector.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenMetaverse;
+
+namespace OpenSim.Region.Framework.Interfaces
+{
+    public interface IActivityDetector
+    {
+        void AgentIsAZombie(UUID agentID);
+    }
+}
