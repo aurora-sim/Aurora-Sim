4e770f621d96c822e527fd68651702a82c707c50
RevolutionSmythe <asdfisbetterthanjkl_AT_gmail.com>
2011-05-15 22:47:24 -0500

    Fix a bug where the correct threat level was not being found.

--- Aurora/AuroraDotNetEngine/ScriptProtectionModule.cs
+++ Aurora/AuroraDotNetEngine/ScriptProtectionModule.cs
@@ -178,11 +178,11 @@ namespace Aurora.ScriptEngine.AuroraDotNetEngine
                 //
                 if (FunctionPerms == null) // No list = true
                 {
-                    if (m_threatLevel > m_scriptProtectionModule.m_MaxThreatLevel)
+                    if (m_threatLevel > m_scriptProtectionModule.GetThreatLevel ())
                         m_scriptProtectionModule.Error ("Runtime Error: ",
                             String.Format (
                                 "{0} permission denied.  Allowed threat level is {1} but function threat level is {2}.",
-                                function, m_scriptProtectionModule.m_MaxThreatLevel, m_threatLevel));
+                                function, m_scriptProtectionModule.GetThreatLevel (), m_threatLevel));
                 }
                 else
                 {
