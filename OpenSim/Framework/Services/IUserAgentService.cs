b8604a54dbb921c95a71a376b8d7b34db40ff010
RevolutionSmythe <asdfisbetterthanjkl_AT_gmail.com>
2011-07-13 12:07:59 -0500

    Add missing file.

---
+++ OpenSim/Framework/Services/IUserAgentService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Linq;
+using System.Text;
+using OpenSim.Framework;
+using OpenMetaverse;
+
+namespace OpenSim.Services.Interfaces
+{
+    /// <summary>
+    /// HG1.5 only
+    /// </summary>
+    public interface IUserAgentService
+    {
+        // called by login service only
+        bool LoginAgentToGrid (AgentCircuitData agent, GridRegion gatekeeper, GridRegion finalDestination, IPEndPoint clientIP, out string reason);
+        // called by simulators
+        bool LoginAgentToGrid (AgentCircuitData agent, GridRegion gatekeeper, GridRegion finalDestination, out string reason);
+        void LogoutAgent (UUID userID, UUID sessionID);
+        GridRegion GetHomeRegion (UUID userID, out Vector3 position, out Vector3 lookAt);
+        GridRegion GetHomeRegion (AgentCircuitData circuit, out Vector3 position, out Vector3 lookAt);
+        Dictionary<string, object> GetServerURLs (UUID userID);
+
+        string LocateUser (UUID userID);
+        // Tries to get the universal user identifier for the targetUserId
+        // on behalf of the userID
+        string GetUUI (UUID userID, UUID targetUserID);
+
+        // Returns the local friends online
+        List<UUID> StatusNotification (List<string> friends, UUID userID, bool online);
+        //List<UUID> GetOnlineFriends(UUID userID, List<string> friends);
+
+        bool AgentIsComingHome (UUID sessionID, string thisGridExternalName);
+        bool VerifyAgent (UUID sessionID, string token);
+        bool VerifyAgent (AgentCircuitData circuit);
+        bool VerifyClient (UUID sessionID, string reportedIP);
+    }
+}
