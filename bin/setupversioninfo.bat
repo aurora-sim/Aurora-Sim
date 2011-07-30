eeaab5aab01694b5c27fda9244ff90a01345de4d
RevolutionSmythe <asdfisbetterthanjkl_AT_gmail.com>
2011-07-30 17:00:06 -0500

    Fix merge collisions.

--- bin/setupversioninfo.bat
+++ bin/setupversioninfo.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @echo off
 setLocal EnableDelayedExpansion
 IF NOT EXIST ..\.git GOTO DIE
@@ -11,18 +10,4 @@ echo Aurora-%%b %%f>.version
 )
 
 :DIE
-=======
-@echo off
-setLocal EnableDelayedExpansion
-IF NOT EXIST ..\.git GOTO DIE
-
-for /f "tokens=* delims= " %%a in (..\.git\logs\HEAD) do (
-set var=%%a
-)
-for /f "tokens=1-7" %%a in ("%var%") do (
-echo Aurora-%%b %%f>.version
-)
-
-:DIE
->>>>>>> upstream/master
 pause
