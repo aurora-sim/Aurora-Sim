da5dd439d737e1549224d76b609e8471a83180ab
Kristen Mynx <kristen.mynx@gmail.com>
2011-08-08 16:09:57 -0500

    Configurable wind would hardly vary because "Seconds" goes 0-59 "TotalSeconds" will allow the variation.

--- OpenSim/Region/CoreModules/World/Wind/Plugins/ConfigurableWind.cs
+++ OpenSim/Region/CoreModules/World/Wind/Plugins/ConfigurableWind.cs
@@ -113,7 +113,7 @@ namespace OpenSim.Region.CoreModules.World.Wind.Plugins
             // TODO: 
             // * This should probably be based on in-world time.
             // * should probably move all these local variables to class members and constants
-            double time = DateTime.Now.TimeOfDay.Seconds / 86400.0f;
+            double time = DateTime.Now.TimeOfDay.TotalSeconds / 86400.0f;
 
             double theta = time * (2 * Math.PI) * m_rateChange;
 
