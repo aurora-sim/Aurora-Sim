1c59a7cdaf674ff8f0494b9e02a856a1b03f3772
Skidz Tweak <skidz.tweak@gmail.com>
2011-12-21 09:42:05 -0600

    added 'ISWIN' compiling condition when you compile on windows USEING COMPILE BAT FILE. This will allow us to place compiler conditionals in the code like '#if (!ISWIN)'. NOTE: If compiling using Visual studios (NOT BAT FILE) it will NOT have the 'ISWIN' compiling condition, so it will run the mono code by default (which is not a big deal). You can add it in under a project properties under the build tab by giving 'Conditional compilation symbols' a value of ISWIN. I would have placed this in the prebuild.xml file, but then when mono prebuilt it would have it as well and would break this. We might want to consider using a seperate prebuild.xml file for windows, but, that kinda goes against the entire concept of using the prebuild.exe

--- runprebuildX64.vs2010.bat
+++ runprebuildX64.vs2010.bat
@@ -1,3 +1,3 @@
 bin\Prebuild.exe /target vs2010 /targetframework v3_5
-echo C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild Aurora.sln /p:Platform="x64" > compilex64.vs2010.bat
+echo C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild Aurora.sln /p:Platform="x64" > compilex64.vs2010.bat /p:DefineConstants=ISWIN
 
