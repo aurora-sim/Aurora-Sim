3ecdfe7ffb1b8a62207fa8179d8bde4bb4e2b956
Revolution Smythe <asdfisbetterthanjkl@gmail.com>
2011-12-25 18:32:15 -0500

    Clean up the notification service loading a bit so that it matches the console loading and add the missing files.

---
+++ OpenSim/Framework/Services/INotificationService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nini.Config;
+
+namespace OpenSim.Framework
+{
+    public enum AlertLevel
+    {
+        Low,
+        Normal,
+        Priority,
+        Immediate
+    }
+
+    public interface INotificationService
+    {
+        #region Init
+
+        void Init(IConfigSource config, IRegistryCore registry);
+
+        #endregion
+
+        #region Notifications
+
+        void AddNotification(AlertLevel level, string message);
+        void AddNotification(AlertLevel level, string message, string identifer);
+
+        #endregion
+    }
+}
---
+++ OpenSim/Services/NotificationService/BaseService.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Aurora.Framework;
+using Aurora.Simulation.Base;
+using log4net;
+using log4net.Appender;
+using log4net.Config;
+using log4net.Core;
+using log4net.Repository;
+using Nini.Config;
+using OpenSim.Framework;
+
+namespace OpenSim.Services
+{
+    public class BaseService : IApplicationPlugin
+    {
+        protected static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        protected OpenSimAppender m_consoleAppender;
+
+        #region IApplicationPlugin Members
+
+        public string Name
+        {
+            get { return "BaseNotificationService"; }
+        }
+
+        public void PreStartup(ISimulationBase simBase)
+        {
+            SetUpConsole(simBase.ConfigSource, simBase.ApplicationRegistry);
+        }
+
+        public void Initialize(ISimulationBase simBase)
+        {
+        }
+
+        private void SetUpConsole(IConfigSource config, IRegistryCore registry)
+        {
+            List<ICommandConsole> Plugins = AuroraModuleLoader.PickupModules<ICommandConsole>();
+            foreach (ICommandConsole plugin in Plugins)
+            {
+                plugin.Initialize(config, registry.RequestModuleInterface<ISimulationBase>());
+            }
+
+            List<INotificationService> NotificationPlugins = AuroraModuleLoader.PickupModules<INotificationService>();
+            foreach (INotificationService plugin in NotificationPlugins)
+            {
+                plugin.Init(config, registry);
+            }
+
+            MainConsole.Instance = registry.RequestModuleInterface<ICommandConsole>();
+            if (MainConsole.Instance == null)
+            {
+                MainConsole.Instance.Info("[Console]: No Console located");
+                return;
+            }
+            ILoggerRepository repository = LogManager.GetRepository();
+            IAppender[] appenders = repository.GetAppenders();
+            foreach (IAppender appender in appenders)
+            {
+                if (appender.Name == "Console")
+                {
+                    m_consoleAppender = (OpenSimAppender)appender;
+                    break;
+                }
+            }
+
+            if (null != m_consoleAppender)
+            {
+                m_consoleAppender.Console = MainConsole.Instance;
+                // If there is no threshold set then the threshold is effectively everything.
+                if (null == m_consoleAppender.Threshold)
+                    m_consoleAppender.Threshold = Level.All;
+                repository.Threshold = m_consoleAppender.Threshold;
+                foreach (ILogger log in repository.GetCurrentLoggers())
+                {
+                    log.Level = m_consoleAppender.Threshold;
+                }
+            }
+            IAppender logFileAppender = null;
+            foreach (IAppender appender in appenders)
+            {
+                if (appender.Name == "LogFileAppender")
+                {
+                    logFileAppender = appender;
+                }
+            }
+
+            if (logFileAppender != null)
+            {
+                if (logFileAppender is FileAppender)
+                {
+                    FileAppender appender = (FileAppender)logFileAppender;
+                    IConfig startupConfig = config.Configs["Startup"];
+                    string fileName = startupConfig.GetString("LogFile", String.Empty);
+                    if (fileName != String.Empty)
+                    {
+                        appender.File = fileName;
+                        appender.ActivateOptions();
+                    }
+                }
+            }
+
+            if (m_consoleAppender != null)
+                MainConsole.Instance.Fatal(String.Format("[Console]: Console log level is {0}", m_consoleAppender.Threshold));
+
+            MainConsole.Instance.Commands.AddCommand("set log level", "set log level [level]", "Set the console logging level", HandleLogLevel);
+        }
+
+        public void PostInitialise()
+        {
+        }
+
+        public void Start()
+        {
+        }
+
+        public void PostStart()
+        {
+        }
+
+        public void Close()
+        {
+        }
+
+        public void ReloadConfiguration(IConfigSource m_config)
+        {
+        }
+
+        #endregion
+
+        #region Console Commands
+
+        private void HandleLogLevel(string[] cmd)
+        {
+            if (null == m_consoleAppender)
+            {
+                MainConsole.Instance.Fatal("No appender named Console found (see the log4net config file for this executable)!");
+                return;
+            }
+
+            string rawLevel = cmd[3];
+
+            ILoggerRepository repository = LogManager.GetRepository();
+            Level consoleLevel = repository.LevelMap[rawLevel];
+            if (consoleLevel != null)
+            {
+                m_consoleAppender.Threshold = consoleLevel;
+                repository.Threshold = consoleLevel;
+                foreach (ILogger log in repository.GetCurrentLoggers())
+                {
+                    log.Level = consoleLevel;
+                }
+            }
+            else
+            {
+                string forms = "";
+                for (int i = 0; i < repository.LevelMap.AllLevels.Count; i++)
+                {
+                    forms += repository.LevelMap.AllLevels[i].Name;
+                    if (i + 1 != repository.LevelMap.AllLevels.Count)
+                        forms += ", ";
+                }
+                MainConsole.Instance.Fatal(
+                    String.Format(
+                        "{0} is not a valid logging level.  Valid logging levels are " + forms,
+                        rawLevel));
+            }
+            MainConsole.Instance.Fatal(String.Format("Console log level is {0}", m_consoleAppender.Threshold));
+        }
+
+        #endregion
+
+        #region INotificationService Members
+
+        public void AddNotification(AlertLevel level, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddNotification(AlertLevel level, string message, string identifer)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
