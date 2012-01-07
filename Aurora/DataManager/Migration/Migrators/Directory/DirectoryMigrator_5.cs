374271c1b6c4ab98267e96cfc340f49e78ddda9c
SignpostMarv <github@signpostmarv.name>
2012-01-07 20:10:52 +0000

    Switching back to signed ints for now

--- Aurora/DataManager/Migration/Migrators/Directory/DirectoryMigrator_5.cs
+++ Aurora/DataManager/Migration/Migrators/Directory/DirectoryMigrator_5.cs
@@ -41,45 +41,21 @@ namespace Aurora.DataManager.Migration.Migrators
 
             schema = new List<Rec<string, ColumnDefinition[]>>();
 
-            AddSchema("searchparcel", ColDefs(ColDef("RegionID", ColumnTypes.String50),
-                                              ColDef("ParcelID", ColumnTypes.String50, true),
-                                              ColDef("LocalID", ColumnTypes.String50),
-                                              ColDef("LandingX", ColumnTypes.String50),
-                                              ColDef("LandingY", ColumnTypes.String50),
-                                              ColDef("LandingZ", ColumnTypes.String50),
-                                              ColDef("Name", ColumnTypes.String50),
-                                              ColDef("Description", ColumnTypes.String255),
-                                              ColDef("Flags", ColumnTypes.String50),
-                                              ColDef("Dwell", ColumnTypes.String50),
-                                              ColDef("InfoUUID", ColumnTypes.String50),
-                                              ColDef("ForSale", ColumnTypes.String50),
-                                              ColDef("SalePrice", ColumnTypes.String50),
-                                              ColDef("Auction", ColumnTypes.String50),
-                                              ColDef("Area", ColumnTypes.String50),
-                                              ColDef("EstateID", ColumnTypes.String50),
-                                              ColDef("Maturity", ColumnTypes.String50),
-                                              ColDef("OwnerID", ColumnTypes.String50),
-                                              ColDef("GroupID", ColumnTypes.String50),
-                                              ColDef("ShowInSearch", ColumnTypes.String50),
-                                              ColDef("SnapshotID", ColumnTypes.String50),
-                                              ColDef("Bitmap", ColumnTypes.LongText),
-                                              ColDef("Category", ColumnTypes.String50)));
-
             AddSchema("events", ColDefs(
                 ColDef("EOwnerID", ColumnTypes.String50),
                 ColDef("EName", ColumnTypes.String50),
-                ColDef("EID", ColumnTypes.UInteger11, true),
+                ColDef("EID", ColumnTypes.Integer11, true),
                 ColDef("ECreatorID", ColumnTypes.String50),
                 ColDef("ECategory", ColumnTypes.String50),
                 ColDef("EDesc", ColumnTypes.String50),
-                ColDef("EDate", ColumnTypes.String50),
-                ColDef("ECoverCharge", ColumnTypes.UInteger11),
-                ColDef("ECoverAmount", ColumnTypes.UInteger11),
+                ColDef("EDate", ColumnTypes.Integer11),
+                ColDef("ECoverCharge", ColumnTypes.Integer11),
+                ColDef("ECoverAmount", ColumnTypes.Integer11),
                 ColDef("ESimName", ColumnTypes.String50),
                 ColDef("EGlobalPos", ColumnTypes.String50),
-                ColDef("EFlags", ColumnTypes.UInteger11),
+                ColDef("EFlags", ColumnTypes.Integer11),
                 ColDef("EMature", ColumnTypes.Integer11),
-                ColDef("EDuration", ColumnTypes.UInteger11)
+                ColDef("EDuration", ColumnTypes.Integer11)
             ));
         }
 
