fd9cfc83e45b27bc14045a151aecc2e075842ed1
SkidzTweak <skidz.tweak@gmail.com>
2012-04-11 20:48:13 -0700

    Quick fix to push. Pressed ctrl+z one to many times.

--- Aurora/Services/DataService/Connectors/Database/Asset/LocalAssetBlackholeConnector.cs
+++ Aurora/Services/DataService/Connectors/Database/Asset/LocalAssetBlackholeConnector.cs
@@ -137,7 +137,7 @@ namespace Aurora.Services.DataService.Connectors.Database.Asset
                 DataManager.DataManager.RegisterPlugin(this);
                 try
                 {
-                    needsConversion = (m_Gd.Query(new string[1] { "id" }, "assets", null, null, null, 1).Count >= 1);
+                    needsConversion = (m_Gd.Query(new string[1] { "id" }, "assets", null, null, 0, 1).Count >= 1);
                 }
                 catch
                 {
