fe4dbd675fc82359d052227a7aaf8387cedce5c4
SignpostMarv <github@signpostmarv.name>
2012-04-18 09:38:22 +0200

    applying possible fix for when not running prebuild.sh from a git clone.

--- runprebuild.sh
+++ runprebuild.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mono bin/Prebuild.exe /target vs2008 /targetframework v3_5
-git log --pretty=format:"Aurora (%cd.%h)" --date=short -n 1 > bin/.version
+if [ -d ".git" ]; then git log --pretty=format:"Aurora (%cd.%h)" --date=short -n 1 > bin/.version; fi
