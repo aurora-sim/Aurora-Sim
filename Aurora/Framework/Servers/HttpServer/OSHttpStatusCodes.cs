aca2d279a9ea781452dfc7723758975e0083e0c6
SignpostMarv <github@signpostmarv.name>
2012-05-24 15:44:18 +0100

    whitespace/newline tweak

--- Aurora/Framework/Servers/HttpServer/OSHttpStatusCodes.cs
+++ Aurora/Framework/Servers/HttpServer/OSHttpStatusCodes.cs
@@ -28,8 +28,7 @@
 namespace Aurora.Framework.Servers.HttpServer
 {
     /// <summary>
-    ///   HTTP status codes (almost) as defined by W3C in
-    ///   http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+    /// HTTP status codes (almost) as defined by W3C in http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
     /// </summary>
     public enum OSHttpStatusCode
     {
