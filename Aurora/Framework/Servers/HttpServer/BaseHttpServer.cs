6ee4adc90d72d7505766781c7fce6344079051df
JAllard <JuniorAllard@gmail.com>
2012-06-16 17:30:03 +0200

    Change reusecontext to false only if we caught a socket exception, so we might be able to use some script services as WebUI login

--- Aurora/Framework/Servers/HttpServer/BaseHttpServer.cs
+++ Aurora/Framework/Servers/HttpServer/BaseHttpServer.cs
@@ -483,12 +483,12 @@ namespace Aurora.Framework.Servers.HttpServer
                     //
                     try
                     {
-                        response.ReuseContext = false;
+                        response.ReuseContext = true;
                         response.Send();
                     }
                     catch (SocketException e)
                     {
-                        // This has to be here to prevent a Linux/Mono crash
+                        response.ReuseContext = false; // This has to be here to prevent a Linux/Mono crash
                         MainConsole.Instance.WarnFormat("[BASE HTTP SERVER]: XmlRpcRequest issue {0}.\nNOTE: this may be spurious on Linux.", e);
                     }
                     catch (IOException e)
