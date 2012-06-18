cd3a0cf7d1aac13b3edeb708818651fc23b332c6
Matthew Beardmore <asdfisbetterthanjkl@gmail.com>
2012-06-18 16:27:14 -0300

    Fix compiling straight from runprebuild.bat.

--- runprebuild.bat
+++ runprebuild.bat
@@ -111,7 +111,7 @@ if %configuration%==release set cfg=/p:Configuration=Release
 if %configuration%==debug set cfg=/p:Configuration=Debug
 set filename=Compile.VS%vstudio%.net%framework%.%bits%.%configuration%.bat
 
-echo %fpath% Aurora.sln %args% %cfg% > %filename% /p:DefineConstants="ISWIN%conditionals%"
+echo %fpath% Aurora.sln %args% %cfg% > %filename% /p:DefineConstants="ISWIN;%conditionals%"
 
 echo.
 set /p compile_at_end="Done, %filename% created. Compile now? (y,n) [%compile_at_end%]"
