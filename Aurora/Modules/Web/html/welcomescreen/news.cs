0be3a0c6572b76bbae5c25cd5881a6ebe632165f
Revolution Smythe <asdfisbetterthanjkl_@_gmail.com>
2012-07-13 13:00:17 -0400

    Add news_list.html to give a list of news items (akin to the welcomescreen news box).

--- Aurora/Modules/Web/html/welcomescreen/news.cs
+++ Aurora/Modules/Web/html/welcomescreen/news.cs
@@ -17,7 +17,8 @@ namespace Aurora.Modules.Web
             {
                 return new[]
                        {
-                           "html/welcomescreen/news.html"
+                           "html/welcomescreen/news.html",
+                           "html/news_list.html"
                        };
             }
         }
@@ -28,11 +29,26 @@ namespace Aurora.Modules.Web
         public Dictionary<string, object> Fill(WebInterface webInterface, string filename, OSHttpRequest httpRequest,
             OSHttpResponse httpResponse, Dictionary<string, object> requestParameters, ITranslator translator)
         {
+            IGenericsConnector connector = Aurora.DataManager.DataManager.RequestPlugin<IGenericsConnector>();
             var vars = new Dictionary<string, object>();
 
             vars.Add("News", translator.GetTranslatedString("News"));
+            vars.Add("NewsDateText", translator.GetTranslatedString("NewsDateText"));
+            vars.Add("NewsTitleText", translator.GetTranslatedString("NewsTitleText"));
+            vars.Add("CurrentPageText", translator.GetTranslatedString("CurrentPageText"));
 
-            IGenericsConnector connector = Aurora.DataManager.DataManager.RequestPlugin<IGenericsConnector>();
+            uint amountPerQuery = 10;
+            int start = httpRequest.Query.ContainsKey("Start") ? int.Parse(httpRequest.Query["Start"].ToString()) : 0;
+            uint count = (uint)connector.GetGenericCount(UUID.Zero, "WebGridNews");
+            int maxPages = (int)(count / amountPerQuery) - 1;
+
+            if (start == -1)
+                start = (int)(maxPages < 0 ? 0 : maxPages);
+
+            vars.Add("CurrentPage", start);
+            vars.Add("NextOne", start + 1 > maxPages ? start : start + 1);
+            vars.Add("BackOne", start - 1 < 0 ? 0 : start - 1);
+
             var newsItems = connector.GetGenerics<GridNewsItem>(UUID.Zero, "WebGridNews");
             if (newsItems.Count == 0)
                 newsItems.Add(GridNewsItem.NoNewsItem);
