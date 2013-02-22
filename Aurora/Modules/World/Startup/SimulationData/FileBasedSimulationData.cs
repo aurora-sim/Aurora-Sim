13d4c650314166d10bc70c0be12189c5093cefea
Revolution Smythe <asdfisbetterthanjkl@gmail.com>
2013-02-21 22:41:47 -0500

    Save the region info into the .abackup file.

--- Aurora/Modules/World/Startup/SimulationData/FileBasedSimulationData.cs
+++ Aurora/Modules/World/Startup/SimulationData/FileBasedSimulationData.cs
@@ -503,6 +503,10 @@ namespace Aurora.Modules.Startup.FileBasedSimulationData
                 writer.WriteDir("newstylewater");
                 writer.WriteDir("newstylerevertwater");
 
+                writer.WriteDir("regioninfo");
+                byte[] regionData = OSDParser.SerializeLLSDBinary(m_scene.RegionInfo.PackRegionInfoData());
+                writer.WriteFile("regioninfo/regioninfo", regionData);
+
                 ITerrainModule tModule = m_scene.RequestModuleInterface<ITerrainModule>();
                 if (tModule != null)
                 {
