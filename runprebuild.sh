f1d4dfadaa4a86a0000e366e83c0875ac5e904c3
Revolution Smythe <asdfisbetterthanjkl@gmail.com>
2013-03-13 18:23:44 -0400

    Add missing runprebuild file.

---
+++ runprebuild.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo Configureing Aurora-Sim
+wait 3
+mono bin/Prebuild.exe /target vs2010 /targetframework v4_0 /conditionals NET_4_0 LINUX
+if [ -d ".git" ]; then git log --pretty=format:"Aurora (%cd.%h)" --date=short -n 1 > bin/.version; fi
+wait 3
+echo Building Aurora-Sim 
+xbuild /property:DefineConstants="LINUX NET_4_0"
+echo Finished Building Aurora
+echo Thank you for choosing Aurora-Sim
+echo Please report any errors to out Mantis Bug Tracker http://mantis.aurora-sim.org/
+wait 5
