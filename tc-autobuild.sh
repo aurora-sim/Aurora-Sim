369be0195f1cfac0ce173497f77235c272947feb
Revolution Smythe <asdfisbetterthanjkl@gmail.com>
2013-04-07 13:13:07 -0400

    Add the LINUX compilation def for TC builds.

--- tc-autobuild.sh
+++ tc-autobuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mono bin/Prebuild.exe /target vs2010 /targetframework v4_0 /conditionals NET_4_0
+mono bin/Prebuild.exe /target vs2010 /targetframework v4_0 /conditionals "LINUX;NET_4_0"
 
 if [ -d ".git" ]; then git log --pretty=format:"Aurora (%cd.%h)" --date=short -n 1 > bin/.version; fi
 
